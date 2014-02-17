065ec58cb5377e3bf0aa350a4e5a94f96648fb59
David Heiniger <drheiniger@gmail.com>
2014-02-16 21:39:45 -0600

    Test

--- workspace/BattlePoker_Android/gen/com/example/battlepoker_android/R.java
+++ workspace/BattlePoker_Android/gen/com/example/battlepoker_android/R.java
@@ -240,8 +240,7 @@ containing a value of this type.
         public static final int bad_guy_unlocked=0x7f020004;
         public static final int bad_guy_unlocked_1=0x7f020005;
         public static final int ic_launcher=0x7f020006;
-        public static final int player=0x7f020007;
-        public static final int player_1=0x7f020008;
+        public static final int player_1=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f040016;
